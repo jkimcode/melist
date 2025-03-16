02d61ac1ba74e8886ad3c9b46edebf251767a94a
work <jinhyuk.kim@proton.me>
2025-03-16 14:17:14 -0700

    work on edit pg

--- src/components/Melist.tsx
+++ src/components/Melist.tsx
@@ -1,4 +1,4 @@
-import { HeartIcon } from "@heroicons/react/24/outline";
+import { ExclamationCircleIcon, HeartIcon, PlusCircleIcon, PlusIcon } from "@heroicons/react/24/outline";
 import { ProductDetails } from "../common/types";
 import { SetStateAction } from "react";
 import { PencilSquareIcon } from "@heroicons/react/24/outline";
@@ -15,6 +15,7 @@ function Melist({ displayMode, setHoveredProduct, setClickedProduct } : MelistPr
     if (displayMode == "condensed") return <MelistCondensedView />
     if (displayMode == "profile" && setHoveredProduct) return <MelistProfileView setHovered={setHoveredProduct} />
     if (displayMode == "my" && setClickedProduct) return <MelistMyView setClicked={setClickedProduct} />
+    if (displayMode == "edit") return <MelistEditView />
 }
 
 function MelistCondensedView() {
@@ -173,6 +174,77 @@ function MelistMyView({ setClicked } : { setClicked: clickedProductSetter }) {
             </div>
         </div>
     )
+}
+
+function MelistEditView() {
+    return (
+        <div className="bg-gray-100 p-6 flex flex-col gap-8 rounded-xl w-sm">
+            {/* header */}
+            <div className="flex gap-4">
+                <div className="bg-white rounded-full size-12" />
+                <div>
+                    <div className="font-bold text-xl">Kylie Jenner</div>
+                    <div className="text-xs">24 products</div>
+                </div>
+            </div>
+
+            {/* products */}
+            <div>
+                {/* featured */}
+                <div className="flex flex-col gap-2">
+                    <div className="flex">
+                        <div className="bg-white size-14 rounded-l-md"></div>
+                        <div className="bg-gray-200 w-full rounded-r-md"></div>
+                    </div>
+                    <div className="flex">
+                        <div className="bg-white size-14 rounded-l-md"></div>
+                        <div className="bg-gray-200 w-full rounded-r-md"></div>
+                    </div>
+                    <div className="flex">
+                        <div className="bg-white size-14 rounded-l-md"></div>
+                        <div className="bg-gray-200 w-full rounded-r-md"></div>
+                    </div>
+                </div>
+                <div className="outline-dotted py-1 px-2 text-xs mt-2 flex items-center">
+                    <ExclamationCircleIcon className="size-4 mr-1" />
+                    featured section can have max. of 3 products
+                </div>
+
+                {/* more products */}
+                <div className="mt-6">
+                    <div className="font-semibold">winter faves</div>
+                    <div className="flex flex-col gap-2 mt-2">
+                        <div className="flex">
+                            <div className="bg-white size-14 rounded-l-md"></div>
+                            <div className="bg-gray-200 w-full rounded-r-md"></div>
+                        </div>
+                        <div className="flex">
+                            <div className="bg-white size-14 rounded-l-md"></div>
+                            <div className="bg-gray-200 w-full rounded-r-md"></div>
+                        </div>
+                        <div className="flex">
+                            <div className="bg-white size-14 rounded-l-md"></div>
+                            <div className="bg-gray-200 w-full rounded-r-md"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="outline-dotted py-1 px-2 text-xs mt-2 flex items-center hover:bg-gray-200">
+                    <PlusCircleIcon className="size-4 mr-1" />
+                    add new
+                </div>
+
+                <div className="outline-dotted py-2 px-2 text-xs mt-6 flex items-center hover:bg-gray-200">
+                    <PlusIcon className="size-4 mr-1" />
+                    add section
+                </div>
+            </div>
+
+            {/* buttons */}
+            <div className="flex gap-4">
+                <div className="py-4 w-full bg-gray-200 flex justify-center items-center font-bold"><HeartIcon className="size-5 stroke-2" /></div>
+            </div>
+        </div>
+    )   
 }
 
 export default Melist
--- src/pages/app/Edit.tsx
+++ src/pages/app/Edit.tsx
@@ -1,7 +1,40 @@
+import { ArrowRightIcon } from "@heroicons/react/24/outline"
+import Melist from "../../components/Melist"
+import { useState } from "react"
 
 function Edit() {
+    const [page, setPage] = useState("menu")
     return (
-        <>Edit</>
+        <div className="mx-auto max-w-5xl p-4">
+            <div className="flex justify-center">
+                <div className="flex flex-col">
+                    <div className="font-extrabold text-3xl mt-16 justify-self-start">Edit</div>
+                    <div className="mt-4 flex gap-8">
+                        <Melist displayMode="edit" />
+                        <div className={`bg-gray-100 h-fit p-8 rounded-md items-center w-lg text-lg transition-[width] duration-100`}>
+                            <div className="font-medium text-sm">What would you like to do?</div>
+                            <div className="flex flex-col gap-2 mt-6">
+                                <div 
+                                    className="p-6 bg-gray-200 font-semibold text-sm flex justify-between hover:bg-gray-300"
+                                    onClick={() => {}}
+                                >
+                                    <div>Add a product</div>
+                                    <ArrowRightIcon className="size-5" />
+                                </div>
+                                <div className="p-6 bg-gray-200 font-semibold text-sm flex justify-between hover:bg-gray-300">
+                                    Customize colors / background
+                                    <ArrowRightIcon className="size-5" />
+                                </div>
+                                <div className="p-6 bg-gray-200 font-semibold text-sm flex justify-between hover:bg-gray-300">
+                                    Edit / remove a product
+                                    <ArrowRightIcon className="size-5" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
