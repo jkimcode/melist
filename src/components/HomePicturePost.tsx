3e785d0aa14e0b871154eb31b44e4ed5232ccc0b
work <jinhyuk.kim@proton.me>
2025-04-01 12:55:38 -0700

    home picture post

--- src/components/HomePicturePost.tsx
+++ src/components/HomePicturePost.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react"
+import { ProductData } from "../common/types"
+import { fetchProductImageUrl } from "../supabase/storage/storage"
 
-function HomePicturePost() {
+interface HomePicturePostProps {
+    productDetails: ProductData
+}
+function HomePicturePost({ productDetails } : HomePicturePostProps) {
+    const [imageUrl, setImageUrl] = useState<string | null>(null)
+    const getImage = async () => {
+        const url = await fetchProductImageUrl(productDetails.id)
+        if (url) setImageUrl(url)
+    }
+    useEffect(() => {
+        getImage()
+    },[])
     return (
-        <>
-            <div className="bg-gray-50 rounded-t-md h-70 w-100"></div>
-            <div className="bg-gray-100 rounded-xs h-10 w-100"></div>
-        </>
+        <div>
+            <img className="h-70 w-100 object-fit" src={imageUrl} />
+            <div className="bg-gray-100 rounded-xs h-10 w-100">
+                {productDetails.product_name}
+            </div>
+        </div>
     )
 }
 
 export default HomePicturePost
+
+
--- src/pages/app/Home.tsx
+++ src/pages/app/Home.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react"
 import HomePicturePost from "../../components/HomePicturePost"
 import HomePost from "../../components/HomePost"
 import { supabase } from "../../supabase/client"
-import { HomeProfile, HomeTag } from "../../common/types"
+import { HomeProfile, HomeTag, ProductData } from "../../common/types"
 import Melist from "../../components/Melist"
+import { fetchProductById } from "../../supabase/api/m_product"
 
 function Home() {
     const [discoverProfiles, setDiscoverProfiles] = useState<HomeProfile[]>([])
+    const [discoverProducts, setDiscoverProducts] = useState<ProductData[]>([])
 
     const getDiscoverProfiles = async () => {
         const { data, error } = await supabase.from("top_followers").select("*")
@@ -54,10 +56,36 @@ function Home() {
         }
 
         setDiscoverProfiles(formattedProfiles)
-        
     }
+
+    const getDiscoverProducts = async () => {
+        const { data, error } = await supabase.from("top_products").select("*")
+        if (error) {
+            console.log("error fetch top products", error)
+            return 
+        }
+
+        const discoverProducts: ProductData[] = []
+        for (let i = 0; i < data.length; i++) {
+            const product = await fetchProductById(data[i].m_product_id)
+            if (!product) continue
+            discoverProducts.push({
+                id: product.id,
+                include_link_if_present: product.include_link_if_present,
+                rank_within_section: null,
+                reaction: null,
+                section_id: product.section_id,
+                user_id: product.user_id,
+                product_name: product.product_name
+            })
+        }
+        
+        setDiscoverProducts(discoverProducts)
+    }
+
     useEffect(() => {
         getDiscoverProfiles()
+        getDiscoverProducts()
     },[])
     return (
         <div className="mx-auto max-w-5xl p-4">
@@ -70,8 +98,10 @@ function Home() {
                         <Melist displayMode="home" discoverProfile={profile}  />
                     ))}
                 </div>
-                <div>
-                    <HomePicturePost />
+                <div className="flex flex-col gap-4">
+                    {discoverProducts.map(product => (
+                        <HomePicturePost productDetails={product} />
+                    ))}
                 </div>
             </div>
         </div>
